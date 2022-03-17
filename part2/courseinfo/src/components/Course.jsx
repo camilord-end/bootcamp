77445ce2bfaf51b52dcf05665fbcacc0a8f67a7d
camilord-end <juliancaor10@gmail.com>
2022-03-17 12:30:27 -0500

    2.3*: Course information step8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,40 +1,40 @@
-const Header = ({ id,text }) => <h1 key={id}>{text}</h1>
+const Header = ({ id, text }) => <h1 key={id}>{text}</h1>;
 
 const Total = ({ parts }) => {
-  return (
-    <p>Total of {parts[0].exercises + parts[1].exercises + parts[2].exercises} exercises</p>
-  )
-}
+  const total = parts.reduce((a, b) => {
+    return { exercises: a.exercises + b.exercises };
+  });
+  return <p>Total of {total.exercises} exercises</p>;
+};
 
-const Part = ({ part }) => 
+const Part = ({ part }) => (
   <>
     {part.name} {part.exercises}
   </>
+);
 
-
-
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <>
-      {parts.map((p)=>{
+      {parts.map((p) => {
         return (
           <p key={p.id}>
-            <Part part={p}/>
+            <Part part={p} />
           </p>
-        )
-      })}   
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return (
     <div className="course">
-      <Header text={course.name} id={course.id}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Header text={course.name} id={course.id} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
